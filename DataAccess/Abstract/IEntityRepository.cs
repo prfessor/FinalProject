7052face90a13c9d30bbc3c815dccd000344fe63
prfessor <oguzhanizgi54@gmail.com>
2021-02-07 23:17:03 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Entities.Abstract;
+
+namespace DataAccess.Abstract
+{
+
+    public interface IEntityRepository<T> where T:class,IEntity, new() //generic constraint //class = referans tip olabilir anlamına gelir. //IEntity = 
+                                                                 //IEntity veya IEntity implemente eden bir nesne olabilir
+                                                                 //new yazarak da IEntity nin kendisi hariç sadece implemente edildiği nesneleri yazabiliyor oluyoruz.
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter=null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        //List<T> GeyByCategory(int categoryId);
+
+
+
+        //buradaki t yerine, bu interface in implemente edildiği tüm interfacelerde farklı entitylerin ismi yazılır(product,category vb.)
+        //metodlar da ona göre dizayn edilir ek olarak yazmaya gerek yok.
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product { CategoryId = 1, ProductId = 1, ProductName = "Bardak", UnitPrice = 15, UnitsInStock = 15 },
+                new Product { CategoryId = 1, ProductId = 2, ProductName = "Kamera", UnitPrice = 500, UnitsInStock = 3 },
+                new Product { CategoryId = 1, ProductId = 3, ProductName = "Telefon", UnitPrice = 1500, UnitsInStock = 2 },
+                new Product { CategoryId = 2, ProductId = 4, ProductName = "Klavye", UnitPrice = 150, UnitsInStock = 65 },
+                new Product { CategoryId = 2, ProductId = 5, ProductName = "Fare", UnitPrice = 85, UnitsInStock = 1}
+
+
+
+            };
+
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //***_products.Remove(product);*** //bu kod çalışmaz çünkü fonksiyona gönderilen referansın hangisi olduğı belli değil
+            Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //    { productToDelete = p; }
+            //}
+            
+            productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+	    }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GeyByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList(); 
+        }
+
+        public void Update(Product product)
+        {
+            //gönderdiğim ürün id'sine sahip olan listedeki ürün bul.
+            Product productToUpdate = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+
+}   
+
